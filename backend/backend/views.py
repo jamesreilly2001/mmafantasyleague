d17cbfaac5d6a35d6302592eceac762c090d07ff
reillj38 <james.reilly38@mail.dcu.ie>
2023-02-01 12:40:50 +0000

    sign up not right 3rd year project

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -42,3 +42,19 @@ def logout(request):
 def signUp(request):
 
   return render(request, "signUp.html")
+
+def postsignup(request):
+ 
+ name=request.POST.get('name')
+ email=request.POST.get('email')
+ passw=request.POST.get('pass')
+ try:
+   user=authe.create_user_with_email_and_password(email,passw)
+   uid = user['localId']
+   data={"name":name,"status":"1"}
+   database.child("users").child(uid).child("details").set(data)
+ except:
+   message="Unable to create account try again"
+   return render(request,"signup.html",{"messg":message})
+ 
+ return render(request,"signIn.html")
