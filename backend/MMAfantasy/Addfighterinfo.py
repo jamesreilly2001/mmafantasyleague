f3892382e140c2dc61b39c249fddbdc19d076410
reillj38 <james.reilly38@mail.dcu.ie>
2023-02-07 14:03:44 +0000

    All fighter info posted to database

--- backend/MMAfantasy/Addfighterinfo.py
+++ backend/MMAfantasy/Addfighterinfo.py
@@ -1,46 +1,182 @@
-import requests
 import json
-from MMAfantasy import models
+from firebase_admin import credentials, firestore, initialize_app
 
-
-
-# Replace [YOUR_FIREBASE_PROJECT_ID] and [YOUR_FIREBASE_AUTH_KEY] with your actual Firebase project ID and authentication key
+# Replace [YOUR_FIREBASE_PROJECT_ID] and [YOUR_FIREBASE_AUTH_KEY_FILE_PATH] with your actual Firebase project ID and authentication key file path
 firebase_project_id = 'mma-fantasy-league-94b6e'
-firebase_auth_key = '73a7ce5de6e25f7cac604eac438e25841edadccd'
+cred = credentials.Certificate("C:\\Users\\user\\Downloads\\mma-fantasy-league-94b6e-firebase-adminsdk-g34f1-4d142578ad.json")
+
+# Initialize the app with a service account, granting admin privileges
+initialize_app(cred, {
+  'projectId': firebase_project_id,
+})
+
+# Get a reference to the Firestore database
+db = firestore.client()
 
 # Define the data for each fighter
 fighters = [
     {
-        'name': 'Conor McGregor',
-        'age': 32,
-        'record': '22-5-0',
+        'name': 'Islam Makhachev',
+        'age': 31,
+        'record': '23-1-0',
+        'weight_class': 'Lightweight'
+    },
+    {
+        'name': 'Alexander Volkanovski',
+        'age': 34,
+        'record': '25-1-0',
         'weight_class': 'Featherweight'
     },
     {
-        'name': 'Jon Jones',
-        'age': 34,
-        'record': '26-1-0',
+        'name': 'Yair Rodríguez',
+        'age': 30,
+        'record': '15-3-0',
+        'weight_class': 'Lightweight'
+    },
+    {
+        'name': 'Josh Emmett',
+        'age': 37,
+        'record': '18-2-0',
+        'weight_class': 'Featherweight'
+    },
+    {
+        'name': 'Jack Della Maddalena',
+        'age': 26,
+        'record': '13-2-0',
+        'weight_class': 'Welterweight'
+    },
+    {
+        'name': 'Randy Brown',
+        'age': 32,
+        'record': '16-4-0',
+        'weight_class': 'Welterweight'
+    },
+    {
+        'name': 'Justin Tafa',
+        'age': 29,
+        'record': '5-3-0',
+        'weight_class': 'Heavyweight'
+    },
+    {
+        'name': 'Parker Porter',
+        'age': 37,
+        'record': '13-7-0',
+        'weight_class': 'Heavyweight'
+    },
+    {
+        'name': 'Jimmy Crute',
+        'age': 26,
+        'record': '12-3-0',
         'weight_class': 'Light Heavyweight'
     },
     {
-        'name': 'Khabib Nurmagomedov',
+        'name': 'Alonzo Menifield',
+        'age': 35,
+        'record': '13-3-0',
+        'weight_class': 'Light Heavyweight'
+    },
+    {
+        'name': 'Tyson Pedro',
+        'age': 31,
+        'record': '9-3-0',
+        'weight_class': 'Light Heavyweight'
+    },
+    {
+        'name': 'modestas bukauskas',
+        'age': 28,
+        'record': '13-5-0',
+        'weight_class': 'Light Heavyweight'
+    },
+    {
+        'name': 'Joshua Culibao',
+        'age': 28,
+        'record': '10-1-1',
+        'weight_class': 'Featherweight'
+    },
+    {
+        'name': 'Melsik Baghdasaryan',
+        'age': 31,
+        'record': '7-1-0',
+        'weight_class': 'Featherweight'
+    },
+    {
+        'name': 'Shannon Ross',
+        'age': 33,
+        'record': '13-6-0',
+        'weight_class': 'Flyweight'
+    },
+    {
+        'name': 'Kleydson Rodrigues',
+        'age': 27,
+        'record': '7-2-0',
+        'weight_class': 'Flyweight'
+    },
+    {
+        'name': 'Jamie Mullarkey',
+        'age': 28,
+        'record': '15-5-0',
+        'weight_class': 'Lightweight'
+    },
+    {
+        'name': 'Francis prado',
+        'age': 20,
+        'record': '11-0-0',
+        'weight_class': 'Lightweight'
+    },
+    {
+        'name': 'Jack Jenkins',
+        'age': 29,
+        'record': '10-2-0',
+        'weight_class': 'Featherweight'
+    },
+    {
+        'name': 'Don Shainis',
+        'age': 34,
+        'record': '12-4-0',
+        'weight_class': 'Featherweight'
+    },
+    {
+        'name': 'Loma Lookboonme',
+        'age': 27,
+        'record': '7-3-0',
+        'weight_class': 'Women Strawweight'
+    },
+    {
+        'name': 'Elise Reed',
+        'age': 30,
+        'record': '6-2-0',
+        'weight_class': 'Women Strawweight'
+    },
+    {
+        'name': 'Shane Young',
+        'age': 29,
+        'record': '13-6-0',
+        'weight_class': 'Featherweight'
+    },
+    {
+        'name': 'Blake Bilder',
         'age': 32,
-        'record': '29-0-0',
+        'record': '7-0-1',
+        'weight_class': 'Featherweight'
+    },
+    {
+        'name': 'Zubaira Tukhugov',
+        'age': 32,
+        'record': '20-5-1',
+        'weight_class': 'Lightweight'
+    },
+    {
+        'name': 'Elves Brenner',
+        'age': 25,
+        'record': '13-3-0',
         'weight_class': 'Lightweight'
     }
+
 ]
 
 # Loop through each fighter, and send the data to Firebase Realtime Database
 for fighter in fighters:
     fighter_id = fighter['name'].lower().replace(' ', '_')
-    endpoint_url = f'https://{firebase_project_id}.firebaseio.com/fighters/{fighter_id}.json'
-    headers = {
-        'Authorization': f'Bearer {firebase_auth_key}',
-        'Content-Type': 'application/json'
-    }
-    data = json.dumps(fighter)
-    response = requests.post(endpoint_url, headers=headers, data=data)
-    if response.status_code != 200:
-        print(f'Error inserting data for fighter {fighter_id}: {response.text}')
-    else:
-        print(f'Successfully inserted data for fighter {fighter_id}')
+    doc_ref = db.collection(u'fighters').document(fighter_id)
+    doc_ref.set(fighter)
+    print(f'Successfully inserted data for fighter {fighter_id}')
